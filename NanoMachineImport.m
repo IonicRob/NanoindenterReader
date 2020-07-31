9c343b9abbba254ab653a04ad8b91857ed817ede
IonicRob <62338052+IonicRob@users.noreply.github.com>
2020-07-31 10:51:00 +0100

    Create NanoMachineImport.m

---
+++ NanoMachineImport.m
@@ -0,0 +1,13 @@
+%% NanoMachineImport
+% By Robert J Scales
+% The objective of this code is to choose which machine the data you want
+% to import is from. If it is produced from the Aglient software (e.g. XP
+% or G200) then no changes will be made. However, if it is from another
+% known machine it will convert it into a spreadsheet file of the correct
+% format for NanaoDataCreater.
+
+% Quasistandard trapezoid method
+
+function NanoMachineImport
+
+end
