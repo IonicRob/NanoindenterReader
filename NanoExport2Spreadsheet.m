29631b3439e802e968cb6a5d6695d8187cd35366
IonicRob <62338052+IonicRob@users.noreply.github.com>
2020-08-17 16:13:38 +0100

    Added exporting function

---
+++ NanoExport2Spreadsheet.m
@@ -0,0 +1,63 @@
+%% NanoExport2Spreadsheet by Robert R J Scales
+% "Export" is an option to convert the information stored in the structures
+% from "import" into an Excel spreadsheet. This can allow one to do other
+% analysis that is not currently available to the current version of the
+% code, OR send the code to someone without Matlab (or who lacks the
+% knowledge) so that they can work on it. 
+% It stores the relevent details on the analysis on the first page so that
+% the user knows how the data was treated.
+
+function NanoExport2Spreadsheet(debugON)
+%%
+    title = 'NanoExport2Spreadsheet';
+    fprintf('%s: Started!\n',title); 
+    cd_init = cd; % Initial directory
+    
+    
+    [FileStuctures,~,cd_load] = LoadingFilesFunc(debugON,'off');
+    IDName = FileStuctures{1}.DataIDName;
+    ValueData = FileStuctures{1}.ValueData;
+    ErrorData = FileStuctures{1}.ErrorData;
+    varNames = FileStuctures{1}.varNames;
+    w = FileStuctures{1}.w;
+    ErrorUsed = FileStuctures{1}.ErrorPlotMode;
+    
+    varTypes    = cell(1,size(ValueData,2));
+    varTypes(:) = {'double'};
+    VETemplateTable = table('Size',size(ValueData),'VariableTypes',varTypes,'VariableNames',varNames);
+    
+    ValueTable = VEFillFunc(VETemplateTable,ValueData);
+    ErrorTable = VEFillFunc(VETemplateTable,ErrorData);
+    
+    detailsTableNames = {'DataIDName','w (stdev weighting)','ErrorUsed'};
+    detailsTableTypes = {'string','double','string'};
+    detailsTable = table('Size',[1,length(detailsTableNames)],'VariableTypes',detailsTableTypes,'VariableNames',detailsTableNames);
+    detailsTable(:,1) = table(IDName);
+    detailsTable(:,2) = table(w);
+    detailsTable(:,3) = table(string(ErrorUsed));
+    
+    quest = sprintf('Export the data for "%s" as an Excel spreadsheet?:',IDName);
+    [SavingLocYN,cd_save] = NanoSaveFolderPref(quest,cd_init,cd_load);
+    if ~strcmp(SavingLocYN,'do not save data')
+        cd(cd_save);
+        SaveTime = datestr(datetime('now'),'yyyy-mm-dd-HH-MM');
+        SpreadSheetSaveName = sprintf('%s_%s_Export.xlsx',IDName,SaveTime);
+        writetable(detailsTable,SpreadSheetSaveName,'Sheet','Details');
+        writetable(ValueTable,SpreadSheetSaveName,'Sheet','ValuesAsDoubles');
+        writetable(ErrorTable,SpreadSheetSaveName,'Sheet','ErrorsAsDoubles');
+        fprintf('Auto-exported "%s"!\n',IDName);
+        cd(cd_init)
+    else
+        fprintf('The data for "%s" was not exported!\n',IDName);
+    end
+    
+    fprintf('%s: Completed!\n',title);  
+end
+
+%%
+function OutTable = VEFillFunc(InTable,InData)
+    for i = 1:size(InData,2)
+        InTable(:,i) = table(InData(:,i));
+    end
+    OutTable = InTable;
+end
--- NanoPlotter.m
+++ NanoPlotter.m
@@ -24,7 +24,7 @@ end
 
 % This loads the ".mat" files produced by NanoDataCreater which the user
 % wishes to plot on the same figure.
-[FileStuctures,fileNameList,cd_load] = LoadingFilesFunc(debugON);
+[FileStuctures,~,cd_load] = LoadingFilesFunc(debugON,'on');
 
 % The below uses the function checkStructuresCompat to see if the data
 % analysis done on the files is the same. See function below for further
@@ -100,54 +100,6 @@ end
 
     
 %% Functions
-
-function [FileStuctures,fileNameList,LOC_load] = LoadingFilesFunc(debugON)    
-    % This allows to get the file name and location information for
-    % multiple files.
-    [file,path] = uigetfile('*.mat','Select nanoindentation "mat" files made by "NanoDataCreater" to plot (must all be in one folder):','MultiSelect','on');
-
-    % If no file is selected then the output of the above is an empty
-    % double.
-    if isa(file,'double') == true
-        errordlg('No files selected! Code will terminate!')
-        return
-    end
-    
-    % The loading path is specified by where the files are loaded from.
-    LOC_load = path;
-    
-    % If one file is chosen its file type will be char and not cell, hence
-    % this makes it into a 1x1 cell if true.
-    if isa(file,'char') == true
-        file = cellstr(file);
-    end
-    
-    % This calculates the number of samples the user has chosen based on
-    % the number of files chosen.
-    NumberOfFiles = length(file);
-
-    if debugON == true
-        fprintf('Loading files from "%s"...\n',LOC_load);
-        fprintf('Number of files detected = %d\n',NumberOfFiles);
-    end
-    
-    % This prepares a string array to be filled in with the full filenames
-    % and the name the user wished to label the data with.
-    fileNameList = strings(NumberOfFiles,2);
-    FileStuctures = cell(NumberOfFiles,1);
-    
-    % This fills in fileNameList
-    for i =1:NumberOfFiles
-        filename = fullfile(path,file{i});
-        fileNameList(i,1) = file{i};
-        fileNameList(i,2) = filename;
-        FileStucturesProto = load(filename,'-mat');
-        FileStuctures{i} = FileStucturesProto.dataToSave;
-        if debugON == true
-            fprintf('Loaded file named "%s"\n',file{i});
-        end
-    end
-end
 
 % The below helps keep track whether the user wants to change what the code
 % saves the figures as in terms of file type.
